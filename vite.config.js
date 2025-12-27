7cd1ff15621db3a2a0d1794dd38f61f93c033e8b
Luis Gutierrez <kike123kob@gmail.com>
2025-12-26 23:18:49 -0500

    setup CI/CD in github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/project_3D_developer_portfolio/', // TU_REPO_NAME
   plugins: [react()],
   server: {
     watch: {
