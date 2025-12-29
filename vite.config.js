4d70f0ec130bc8deefa1d1a36ef89e0fa0fcc1a0
Luis Gutierrez <kike123kob@gmail.com>
2025-12-29 06:10:05 -0500

    disable minify config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/project_3D_developer_portfolio/', // TU_REPO_NAME
-  build: { minify: false },
+  //build: { minify: false },
   plugins: [react()],
   server: {
     watch: {
